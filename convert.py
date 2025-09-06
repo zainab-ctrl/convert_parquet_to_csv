6485566ab18b3e9a27974b13461a3f9bf96e7993
zainab-ctrl <zainabzawar132@gmail.com>
2025-09-06 23:13:21 +0500

    Updated file

--- convert.py
+++ convert.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 # read parquet file
-df = pd.read_parquet(r'C:\Users\Zawar Khan\Downloads\yellow_tripdata_2024-01.parquet')
+parquet_file_path = 'C:\Users\Zawar Khan\Downloads\yellow_tripdata_2024-01.parquet'
+df = pd.read_parquet(parquet_file_path)
 
 # change parquet file to csv 
-df.to_csv('yellow_2023_01.csv', index = False)
+name_csv_file = 'yellow_2023_01.csv'
+df.to_csv(name_csv_file, index = False)
